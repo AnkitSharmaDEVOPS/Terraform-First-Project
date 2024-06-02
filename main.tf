986eeadc11f1c13562383442e913aec90b31ac13
AnkitSharma <ankitrial7@gmail.com>
2024-06-02 14:34:39 +0530

    commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "EC2" {
+ ## count = "${var.NOI}"
+  tags = {
+    Name = "EC2 2nd day - ${count.index}"
+  }
+  ami             = var.ami
+  instance_type   = var.instance_type
+  security_groups = var.security_groups
+  key_name        = "TF_practice"
+  ##user_data       = "$file(jenkins.sh)"
+  user_data = var.user_data
+}
---
+++ output.tf
@@ -0,0 +1,5 @@
+output "Public_IP" {
+  description = "Public IP"
+  value       = aws_instance.EC2.public_ip
+
+}
