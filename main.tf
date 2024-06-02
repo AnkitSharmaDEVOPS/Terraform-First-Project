fe756da30813be0290c3c59ad04bc343b13d7dc5
AnkitSharma <ankitrial7@gmail.com>
2024-06-02 14:44:14 +0530

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "EC2" {
  ## count = "${var.NOI}"
   tags = {
-    Name = "EC2 2nd day - ${count.index}"
+    ##Name = "EC2 2nd day - ${count.index}"
+    Name   = "Jenkins Server"
   }
   ami             = var.ami
   instance_type   = var.instance_type
